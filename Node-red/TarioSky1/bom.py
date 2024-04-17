43c3151707e209b776427216a598d0ef99100a16
Hartberg <jonashartberg@gmail.com>
2024-04-17 14:56:32 +0200

    added bom py file for node red

---
+++ Node-red/TarioSky1/bom.py
@@ -0,0 +1,25 @@
+import sys 
+import pandas as pd
+
+#det er det som blir printet som blir sendt til neste ledd i node-red
+
+#sys.argv[1] er stringen etter python3 filnamn.py og i dette tilfelle bilnummer
+inputCarId = int(sys.argv[1])
+
+#les av csv filen 
+df = pd.read_csv('/home/jjhrasberry1/Desktop/TarioSky/IDnumber.csv')
+
+
+def bomImport():
+    #hent hvilken rad bilen er i (gjøres for å holde koden enklere)
+    carRow = df[df['car-ID'] == inputCarId].index[0]
+
+    #sjekker riktig rad og kolonne for den spesifikke bil
+    iselectric = df.iloc[carRow, 1]
+    if (iselectric == 1 or iselectric == 0):
+        print(iselectric)
+        return iselectric
+    else:
+        return "Value not good"
+
+bomImport()
