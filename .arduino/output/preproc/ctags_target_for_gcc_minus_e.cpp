9686a1431405bf04d1d2f47dd8eb996da20e0b7e
Aasebop <145003954+Aasebop@users.noreply.github.com>
2024-04-22 14:00:47 +0200

    varslingssystem in progress

--- .arduino/output/preproc/ctags_target_for_gcc_minus_e.cpp
+++ .arduino/output/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,19 +1,17 @@
 # 1 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
 # 2 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 2
 # 3 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 2
-
 Zumo32U4IMU imu;
-
 char report[120];
 const float conversion_factor = 0.061; // [mg/LSB] akselerometerlesningene konverteres til enheter av g ved å bruke sensitiviteten
 const float g = 8.91; // [m/s^2] tyngdeaksellerasjon
-float accelerationValues[2]; // Array to hold acceleration values
+const int16_t accelerationTreshold = 2;
+float accelerations_m_s2[3];
 
 void setup()
 {
   Wire.begin();
   Serial.begin(9600);
-
   if (!imu.init())
   {
     // Failed to detect the compass.
@@ -21,69 +19,53 @@ void setup()
     while (1)
     {
       Serial.println((reinterpret_cast<const __FlashStringHelper *>(
-# 22 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 3
+# 20 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 3
                     (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 22 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
+# 20 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
                     "Failed to initialize IMU sensors."
-# 22 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 3
+# 20 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 3
                     ); &__c[0];}))
-# 22 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
+# 20 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
                     )));
       delay(100);
     }
   }
-
   imu.enableDefault();
 }
-
 void loop()
 {
 
-  calculate_Acceleration_g(accelerationValues); // Calculate acceleration in g and store in array
+  accelerationValues_m_s2(accelerations_m_s2);
+  Serial.print("ax in m/s^2: ");
+  Serial.print(accelerations_m_s2[0], 4); // ax component in m/s^2
+  Serial.print(", ay in m/s^2: ");
+  Serial.print(accelerations_m_s2[1], 4); // ay component in m/s^2
+  Serial.print(", Total in m/s^2: ");
+  Serial.println(accelerations_m_s2[2], 4); // total vector magnitude in m/s^2
 
-  float acceleration_m_s2 = calculate_Acceleration();
+  displayWarnings();
 
-  // Now print out the acceleration values
-  Serial.print("ax: ");
-  Serial.print(accelerationValues[0], 4); // Print ax with 4 decimal places
-  Serial.print(" g, ay: ");
-  Serial.print(accelerationValues[1], 4); // Print ay with 4 decimal places
-  Serial.println("g ");
-
-  Serial.print((reinterpret_cast<const __FlashStringHelper *>(
-# 44 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 3
-              (__extension__({static const char __c[] __attribute__((__progmem__)) = (
-# 44 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
-              "Total acceleration (m/s^2): "
-# 44 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino" 3
-              ); &__c[0];}))
-# 44 "C:\\Users\\Aaseb\\OneDrive\\Skrivebord\\Repository\\Zumo-Smart-City\\Zumo-car\\Kjoremonster\\Kjoremonster.ino"
-              )));
-  Serial.println(acceleration_m_s2);
-
-  delay(1000);
+  delay(500);
 }
 
-// Calculate acceleration from accelerometer raw data
-// Driter i z-aksen, anntar byen er flat!
-float calculate_Acceleration()
-{
-
-  // Accelerations in g's per axis
-  float ax = imu.a.x * conversion_factor / 1000;
-  float ay = imu.a.y * conversion_factor / 1000;
-
-  float a = sqrtf((ax * ax) + (ay * ay)); // Acceleration in g's total
-  float acceleration = a * g; // Acceleration in m/s^2
-  return acceleration;
-}
-
-// Function to calculate and return acceleration values in an array
-void calculate_Acceleration_g(float accelerationArray[3])
+void accelerationValues_m_s2(float accelerations_m_s2[3])
 {
   imu.read();
 
-  // Convert accelerometer readings from milli-g to g
-  accelerationArray[0] = imu.a.x * conversion_factor / 1000.0; // ax
-  accelerationArray[1] = imu.a.y * conversion_factor / 1000.0; // ay
+  float ax = imu.a.x * conversion_factor / 1000; // Accel x i g's
+  float ay = imu.a.y * conversion_factor / 1000; // Accel y i g's
+  float a = sqrtf((ax * ax) + (ay * ay)); // Acceleration in g's total
+
+  accelerations_m_s2[0] = ax * g; // Accel x i m/s^2
+  accelerations_m_s2[1] = ay * g; // Accel y i m/s^2
+  accelerations_m_s2[2] = a * g; // Accel total i m/s^2
+}
+
+void displayWarnings (){
+  if (accelerations_m_s2[0] < -accelerationTreshold){
+    Serial.println("You are braking to hard, try slowing down earlier");
+  } else if (accelerations_m_s2[0] > accelerationTreshold)
+  {
+    Serial.println("You are speeding too fast, try accelerating slower :)");
+  }
 }
