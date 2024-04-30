a54f0c202d39b61f9d12dad22dc29e9748afdb0a
Hartberg <jonashartberg@gmail.com>
2024-04-30 18:00:35 +0200

    lagt til start på ui

---
+++ Node-red/TarioSky1/getUser.py
@@ -0,0 +1,19 @@
+import sys 
+import pandas as pd
+
+df_user = pd.read_csv(
+    '/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/users.csv')
+
+# i = pd.options.display.max_rows
+
+def getUsers():
+    print(df_user)
+
+
+getUsers()
+
+print(df_user)
+
+print("hei")
+
+
