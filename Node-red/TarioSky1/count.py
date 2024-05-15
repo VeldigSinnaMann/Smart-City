92c547873a32526510a1a52e881c7d2824c038b3
Hartberg <jonashartberg@gmail.com>
2024-05-15 11:47:37 +0200

    system for dymanisk grad intergasjon

--- Node-red/TarioSky1/count.py
+++ Node-red/TarioSky1/count.py
@@ -1,4 +1,3 @@
-#tar inn et skildnummer som sys.argv[1] og sjekker antall ganger over 2m/s^2
 import pandas as pd
 from datetime import datetime
 import sys
@@ -6,12 +5,18 @@ import sys
 licensePlate = sys.argv[1]
 timeNow = datetime.now()
 
-
 df_logg = pd.read_csv('/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/logg.csv')
 
-print(df_logg)
+# Convert timestamp to datetime
+df_logg['timestamp'] = pd.to_datetime(df_logg['timestamp'])
 
-df_logg.sort_values(by=['car-id'])
+# Filter records for the given license plate number
+df_license_plate = df_logg[df_logg['car-id'] == int(licensePlate)]
 
-print(df_logg)
+# Filter records for the last day
+last_day_records = df_license_plate[df_license_plate['timestamp'] >= (timeNow - pd.Timedelta(days=1))]
 
+# Count the number of times the score is over 2 m/s^2
+count_high_scores = (last_day_records['score'] > 2).sum()
+
+print(f"The car with license plate number {licensePlate} had a score over 2 m/s^2 {count_high_scores} times in the last day.")
