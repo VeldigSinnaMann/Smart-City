5b8132f1b62e718f1620f8474e9ff2f3b1b1a8e0
Hartberg <jonashartberg@gmail.com>
2024-05-15 15:41:21 +0200

    dynamisk prising prototype 1

---
+++ Node-red/TarioSky1/countAndGrade.py
@@ -0,0 +1,37 @@
+#denne koden leser av all data logg.csv og gir en rating på hver bil fra 10-0 basert på antall 
+# prosent av tiden de har kjørt over 1.8m/S^2.
+
+import pandas as pd
+from datetime import datetime, timedelta
+import sys
+
+
+licensePlate = int(sys.argv[1])
+timeNow = datetime.now()
+
+df_logg = pd.read_csv('/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/logg.csv')
+df_database = pd.read_csv('/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/IDnumber.csv')
+
+plateNumberList = df_database['car-ID'].tolist()
+
+one_day_ago = timeNow - timedelta(days=1)
+
+def getBreachProsentage(plateNumber, limit):
+    #filtrer ut etter skiltnummer og fjerner logg som er eldre enn 1 dag
+    df_car = df_logg[(df_logg.carId == int(plateNumber)) & (pd.to_datetime(df_logg.timestamp) >= one_day_ago)]
+    numberOfReadings = df_car.shape[0]
+    #fjern de lesingene under grensen
+    df_car = df_car[df_car.score >= limit] 
+    #les av hvor mange brytninger vi har
+    breaches = df_car.shape[0]
+    prosentage = breaches/numberOfReadings
+    return prosentage
+    
+for car in plateNumberList:
+    carProsentage = getBreachProsentage(car, 1.8)
+    carRow = df_database[df_database['car-ID'] == car].index[0]
+    print(carRow)
+    #her formaterer vi fra prosnet til en skala fra en til 10
+    df_database.iloc[carRow, 2] = 10-carProsentage*10
+
+df_database.to_csv('/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/IDnumber.csv', index=False)
--- Node-red/TarioSky1/getPrice.py
+++ Node-red/TarioSky1/getPrice.py
@@ -12,12 +12,12 @@ inputCarId = int(sys.argv[1])
 # les av csv filen
 df_grade = pd.read_csv(
     '/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/IDnumber.csv')
-
-df_bank = pd.read_csv('/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/bankDetail.csv')
+df_bank = pd.read_csv(
+    '/home/jjhrasberry1/Desktop/Zumo-Smart-City/Node-red/TarioSky1/bankDetail.csv')
 
 
 def getCarGradeFromId(carNumber):
-     # hent hvilken rad bilen er i (gjøres for å holde koden enklere)
+    # hent hvilken rad bilen er i (gjøres for å holde koden enklere)
     carRow = df_grade[df_grade['car-ID'] == carNumber].index[0]
 
     # sjekker riktig rad og kolonne for den spesifikke bil.
@@ -34,46 +34,43 @@ def getPriceNow():
     day = f"{today.day:02d}"
     hour = timeToday.hour
 
-    
     # produser riktig url for tiden naa og hent json
     urlString = "https://www.hvakosterstrommen.no/api/v1/prices/" + \
         str(year) + "/" + str(month) + "-" + str(day) + "_NO5.json"
     response = requests.get(urlString)
     response_dict = response.json()
     priceNow = response_dict[hour]['NOK_per_kWh']  # hent ut pris fra json fil
-    
+
     return priceNow
 
+
 def carGradeToDiscount(grade):
-    discount = 1 
-    if grade == 1:
+    discount = 1
+    if grade < 1:
         discount = 1.1
-    elif grade == 2:
+    elif grade < 2:
         discount = 1.05
-    elif grade == 3:
+    elif grade < 3:
         discount = 1.02
-    elif grade == 8:
+    elif grade < 8:
         discount = 0.98
-    elif grade == 9:
+    elif grade < 9:
         discount = 0.95
-    elif grade == 10:
+    elif grade < 10:
         discount = 0.92
     return discount
-        
-        
-        
-     
+
 
 def getPrice():
-    #finn nåtidens pris
+    # finn nåtidens pris
     currentPrice = getPriceNow()
-    #finn kjøremønster karakter
+    # finn kjøremønster karakter
     gradeCar = getCarGradeFromId(inputCarId)
-    #finn prisendring basert på karakter
+    # finn prisendring basert på karakter
     priceChangeRatio = carGradeToDiscount(gradeCar)
 
     # pris til kunde
-    #chargingPrice = currentPrice*priceChangeRatio
+    # chargingPrice = currentPrice*priceChangeRatio
 
     # print er output av function
     print(str(currentPrice) + ", " + str(priceChangeRatio), end="")
