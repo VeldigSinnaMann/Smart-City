e9040f8fe74a67eac18abb48496976d7d4ff0924
Hartberg <jonashartberg@gmail.com>
2024-06-05 13:09:23 +0200

    insertLogg.py fix

--- Node-red/TarioSky1/insertLogg.py
+++ Node-red/TarioSky1/insertLogg.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 
 # Get inputs from command line arguments
-score = round(sys.argv[1], 3) 
+score = round(float(sys.argv[1]), 3) 
 licensePlate = sys.argv[2]
 timeStamp = datetime.now()
 
